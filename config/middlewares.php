64aae7c6c79c6ef31dc2b8cad97a91acfb4b46a6
dule1703 <duledrljaca@yahoo.com>
2025-10-21 17:34:07 +0200

    Modify middlewares.php

--- config/middlewares.php
+++ config/middlewares.php
@@ -11,7 +11,7 @@ use Psr\Log\LoggerInterface;
 return [
     AuthMiddleware::class => \DI\autowire()
         ->constructorParameter('logger', \DI\get(LoggerInterface::class))
-        ->constructorParameter('jwtSecret', \DI\get('jwt.secret'))
+        // ->constructorParameter('jwtSecret', \DI\get('jwt.secret'))
         ->constructorParameter('redirectUrl', '/login'),
 
     LoggingMiddleware::class => \DI\autowire()
